aee5b6b61c5964053289530741a753e058b7dc67
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-01-04 16:02:20 +0100

    refactor(Document): Add initializers for drawing
    Add new init(drawing:) and convenience init() for more flexible drawing initialization

--- Sources/DrawOnImage/Document.swift
+++ Sources/DrawOnImage/Document.swift
@@ -6,7 +6,11 @@ open class DrawableObservableDocument : ObservableObject{
     @Published public var drawing: PKDrawing
     @Published public var drawingBackgroundImage: UIImage?
     
-    public init( ) {
-        self.drawing = PKDrawing()
+    public init( drawing: PKDrawing ) {
+        self.drawing = drawing
+    }
+    
+    public convenience init() {
+        self.init( drawing: PKDrawing() )
     }
 }
