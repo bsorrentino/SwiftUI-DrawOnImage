27ae1a71edad1f8105e8e434cb9c8bbebd8c9048
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-01-04 16:16:17 +0100

    feat(Document): Add observable support for drawing document
    Introduce observable features with conditional compilation support for both @Observable and ObservableObject implementations

--- Sources/DrawOnImage/Document.swift
+++ Sources/DrawOnImage/Document.swift
@@ -3,12 +3,32 @@ import SwiftUI
 import Observation
 import PencilKit
 
+
+#if __USE_OBSERVABLE__
 @Observable
 open class DrawableObservableDocument {
     public var drawing: PKDrawing
     public var drawingBackgroundImage: UIImage?
     
-    public init( ) {
-        self.drawing = PKDrawing()
+    public init( drawing: PKDrawing ) {
+        self.drawing = drawing
+    }
+    
+    public convenience init() {
+        self.init( drawing: PKDrawing() )
     }
 }
+#else
+open class DrawableObservableDocument : ObservableObject {
+    @Published public var drawing: PKDrawing
+    @Published public var drawingBackgroundImage: UIImage?
+    
+    public init( drawing: PKDrawing ) {
+        self.drawing = drawing
+    }
+    
+    public convenience init() {
+        self.init( drawing: PKDrawing() )
+    }
+}
+#endif
